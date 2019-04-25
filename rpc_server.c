2c4d94fddb0a41d067c25a00a06e6485d4026b1d
rajubangarwa <49992533+rajubangarwa@users.noreply.github.com>
2019-04-25 22:10:20 +0530

    Add files via upload

---
+++ rpc_server.c
@@ -0,0 +1,38 @@
+#include "rpc.h"
+#include <stdio.h>
+
+float *
+add_6_svc(values *argp, struct svc_req *rqstp)
+{
+	static float  result;
+	result = argp->num1 + argp->num2;
+	printf("number recieved is:%f    \n",argp->num1 );
+	printf("number recieved is:%f\n",argp->num2 );
+	return &result;
+}
+
+float *
+sub_6_svc(values *argp, struct svc_req *rqstp)
+{
+	static float  result;
+	result = argp->num1 - argp->num2;
+	return &result;
+}
+
+float *
+mul_6_svc(values *argp, struct svc_req *rqstp)
+{
+	static float  result;
+	result = argp->num1 * argp->num2;
+	return &result;
+}
+
+float *
+div_6_svc(values *argp, struct svc_req *rqstp)
+{
+	static float  result;
+	result = argp->num1 / argp->num2;
+	return &result;
+
+
+}
